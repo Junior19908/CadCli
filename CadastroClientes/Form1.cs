2b39639a5fe1dc4bb8a99a546cb15185d5858717
junio <junio@JAVIS>
2022-03-01 22:11:27 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroClientes/Form1.cs
@@ -0,0 +1,10 @@
+namespace CadastroClientes
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
